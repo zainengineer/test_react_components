d225d171698d3349de5814cf1811c3c7dbc10715
Zain ul abidin <zainengineer@gmail.com>
2017-11-12 23:59:12 +1030

    twitter bootstrap: hello world grid working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Grid,Row,Col} from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -13,6 +14,28 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+          <Grid style={{border:1}}>
+              <Row className="show-grid">
+                  <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+                  <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+              </Row>
+
+              <Row className="show-grid">
+                  <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+                  <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+                  <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
+              </Row>
+
+              <Row className="show-grid">
+                  <Col xs={6} xsOffset={6}><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
+              </Row>
+
+              <Row className="show-grid">
+                  <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
+                  <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
+              </Row>
+          </Grid>
       </div>
     );
   }
